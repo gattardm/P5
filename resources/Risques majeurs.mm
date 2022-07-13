24abd64d74bd39232086a03e6fb57c7d121ebf29
gattardm <gerald.attard@orange.fr>
2022-07-13 18:36:27 +0200

    finalisation du SAW

---
+++ resources/Risques majeurs.mm
@@ -0,0 +1,83 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Risques&#xa;majeurs" FOLDED="false" ID="ID_1690205808" CREATED="1657720963200" MODIFIED="1657721210941" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Ouvrage" POSITION="right" ID="ID_1303352680" CREATED="1657721123149" MODIFIED="1657721206311" HGAP_QUANTITY="2.000000357627858 pt" VSHIFT_QUANTITY="-32.24999903887513 pt">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Oeuvre" POSITION="left" ID="ID_1967819703" CREATED="1657721141580" MODIFIED="1657721208605" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="-41.999998748302495 pt">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Ressources" POSITION="right" ID="ID_279517048" CREATED="1657721150412" MODIFIED="1657721171577" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="11.999999642372142 pt">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Planification" POSITION="left" ID="ID_1533745930" CREATED="1657721176604" MODIFIED="1657721210940" HGAP_QUANTITY="9.500000134110447 pt" VSHIFT_QUANTITY="51.74999845772986 pt">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Suivi" POSITION="right" ID="ID_1424967227" CREATED="1657721186292" MODIFIED="1657721200726" HGAP_QUANTITY="5.000000268220894 pt" VSHIFT_QUANTITY="26.99999919533732 pt">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
